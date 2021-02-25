211ec0702fbef7bba9df7cd194994c833c81bf12
szq <szq2904142693@gmail.com>
2021-02-26 01:14:22 +0800

    初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,21 @@
-// pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    name:'szq',
+    students:[
+      {id:1,name:'q',age:23},
+      {id:2,name:'w',age:22},
+      {id:1,name:'e',age:25},
+    ],
+    counter:0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  subclick(){
+    this.setData({
+      counter:this.data.counter-1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  addclick(){
+    this.setData({
+      counter:this.data.counter+1
+    })
   }
 })
